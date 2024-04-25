15986ec2d95100dd40cc30702de3e60bf6d679a2
Shamiq Raza <Shamiqraza169@gmail.com>
2024-04-25 03:47:06 -0700

    variable complete

---
+++ Variable/app.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "shamiqraza169@gmail.com"
+var accountPassword = "expressway"
+accountCity = "karachi"
+let accountstate;
+
+accountEmail = "shamiqrazagmail.com"
+accountPassword = "12121212"
+accountCity = "lahore"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword,accountCity,accountstate])
