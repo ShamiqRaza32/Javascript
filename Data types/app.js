72c22f4093cc343d4ce7057c4e85ffd9634bc641
Shamiq Raza <Shamiqraza169@gmail.com>
2024-04-25 04:26:19 -0700

    Datatypes

---
+++ Data types/app.js
@@ -0,0 +1,27 @@
+"Use Strict"; //Treat all js code as new version
+
+ // alert(3 + 3)  // we are using nodejs; not browser
+ 
+ console.log((3 + 3));
+
+console.log(("Syed Shamiq Raza"));
+
+let name = ("Syed Shamiq Raza")
+let age = 20
+let isLoggedIn = false
+let state; 
+
+
+// number => 2 to power 53
+// bright
+// string
+// boolean => true / false
+// null => standlone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);      // object
+
