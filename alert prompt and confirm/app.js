822b87889f3ba1fd929212a4ede10d3070208b2f
Shamiq Raza <Shamiqraza169@gmail.com>
2024-04-25 03:42:44 -0700

    variable commpleted

---
+++ alert prompt and confirm/app.js
@@ -0,0 +1,24 @@
+alert("This is a message");
+
+let name = prompt("what is your name ?")
+console.log(name);
+
+let deletepost = confirm("Really Delete This Page");
+console.log(deletepost);
+
+// console.log(name);
+
+let deletedepost = confirm("Really Delete This Post");
+
+// console.log(delete post)
+
+if(deletepost){
+
+    //console.log(delete post)
+    console.log("your post has been deleted susscsful");
+}
+
+else{
+    //code to cancel delete of the post 
+    console.log("your post has been deleted");
+}
